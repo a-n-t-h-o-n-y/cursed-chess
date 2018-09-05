e7f2f3632ba2cc7e4fa48e24b08f2fa0a6b7fb60
Anthony Leedom <anthonym.leedom@gmail.com>
2018-09-04 20:05:24 -0500

    Update to new CPPurses interface.

--- src/chess_move_request_event.cpp
+++ src/chess_move_request_event.cpp
@@ -1,23 +1,20 @@
 #include "chess_move_request_event.hpp"
 
-#include <cppurses/system/event_handler.hpp>
+#include <cppurses/widget/widget.hpp>
 
 #include "chessboard_widget.hpp"
 
 namespace chess {
 Chess_move_request_event::Chess_move_request_event(Chessboard_widget* cbw,
                                                    struct Move m)
-    : cppurses::Event{cppurses::Event::Custom,
-                      static_cast<cppurses::Event_handler*>(cbw)},
-      move_{m} {}
+    : cppurses::Event{cppurses::Event::Custom, cbw}, move_{m} {}
 
 bool Chess_move_request_event::send() const {
     static_cast<Chessboard_widget*>(receiver_)->move_request_event(move_);
     return true;
 }
 
-bool Chess_move_request_event::filter_send(
-    cppurses::Event_handler* filter) const {
+bool Chess_move_request_event::filter_send(cppurses::Widget* filter) const {
     return false;
 }
 
